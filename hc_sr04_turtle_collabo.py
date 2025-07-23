444eb195885b1436d03e365607888761e09abf7b
jaemin6 <gumt555@gmail.com>
2025-07-23 11:42:52 +0900

    스켈레톤 코드 작성
    함수 정의

---
+++ hc_sr04_turtle_collabo.py
@@ -0,0 +1,24 @@
+import serial
+import time
+import turtle
+
+# 설정
+PORT = 'COM7'      # 아두이노 연결 포트
+stopdistance = 10  # 멈추는 거리(cm)
+
+# turtle 셋팅
+def setup_turtle():
+
+
+# 시리얼 연결
+def connection_sensor(port='COM7'):
+    
+    
+# 거리 데이터 읽기
+def read_distance():
+    
+    
+    
+    
+# 메인 함수  
+def main():
